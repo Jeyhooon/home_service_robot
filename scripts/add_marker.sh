f35951dce791bf118e936abab4feb37b62942571
jeyhooon <jeyhoon.maskani@gmail.com>
2022-01-09 14:29:21 +0000

    update launch file names

--- scripts/add_marker.sh
+++ scripts/add_marker.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-xterm -e "roslaunch turtlebot_gazebo turtlebot_myworld.launch" &
+xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch" &
 sleep 15
 xterm -e "roslaunch turtlebot_gazebo amcl_demo.launch" &
 sleep 5
--- scripts/pick_objects.sh
+++ scripts/pick_objects.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-xterm -e "roslaunch turtlebot_gazebo turtlebot_myworld.launch" &
+xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch" &
 sleep 15
 xterm -e "roslaunch turtlebot_gazebo amcl_demo.launch" &
 sleep 5
 xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 5
-xterm -e "rosrun pick_objects pick_objects"
+xterm -e "rosrun pick_objects move_robot"
