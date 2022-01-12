630cba985a520f0f527d8bec208fdc2d6fcc4453
Jeyhoon Maskani <31189258+Jeyhooon@users.noreply.github.com>
2022-01-12 11:42:10 +0100

    Update add_marker.sh

--- scripts/add_marker.sh
+++ scripts/add_marker.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 xterm -e "roslaunch add_markers launch_myworld.launch" &
 sleep 15
-xterm -e "roslaunch turtlebot_gazebo amcl_demo.launch" &
+xterm -e "roslaunch add_markers launch_amcl.launch" &
 sleep 5
 xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 5
