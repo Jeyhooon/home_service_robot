802f47831de29c03dfaea61b66245288c731bbb9
Jeyhoon Maskani <31189258+Jeyhooon@users.noreply.github.com>
2022-01-12 11:45:13 +0100

    Update pick_objects.sh

--- scripts/pick_objects.sh
+++ scripts/pick_objects.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 xterm -e "roslaunch add_markers launch_myworld.launch" &
 sleep 15
-xterm -e "roslaunch turtlebot_gazebo amcl_demo.launch" &
+xterm -e "roslaunch add_markers launch_amcl.launch" &
 sleep 5
 xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 5
-xterm -e "rosrun pick_objects move_robot"
+xterm -e "rosrun pick_objects pick_objects"
