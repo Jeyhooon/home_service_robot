c485d6c5c0e3cc6b745dee1eca3caa710ef44874
Jeyhoon Maskani <31189258+Jeyhooon@users.noreply.github.com>
2022-01-12 11:44:30 +0100

    Update home_service.sh

--- scripts/home_service.sh
+++ scripts/home_service.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 xterm -e "roslaunch add_markers launch_myworld.launch" &
 sleep 15
-xterm -e "roslaunch turtlebot_gazebo amcl_demo.launch" &
+xterm -e "roslaunch add_markers launch_amcl.launch" &
 sleep 5
 xterm -e "roslaunch add_markers launch_rviz_config.launch" &
 sleep 5
-xterm -e "rosrun pick_objects move_robot" &
+xterm -e "rosrun pick_objects pick_objects" &
 sleep 10
 xterm -e "roslaunch add_markers launch_add_markers.launch"
